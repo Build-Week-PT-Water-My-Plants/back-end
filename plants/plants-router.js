997fa905ac30b1dfd4ab656ecae68598b3803b03
kim-boyd <kimberlyboyd2@gmail.com>
2020-09-19 00:37:13 -0400

    routers almost done, need to fix a few bugs

--- plants/plants-router.js
+++ plants/plants-router.js
@@ -5,7 +5,7 @@ const restrict = require("../plants/plants-middleware")
 const router = express.Router()
 
 
-// get list of plants (this works)
+// get list of plants
 router.get("/",restrict(), async (req, res, next) => {
     try {
         res.json(await Plants.find())
@@ -14,7 +14,7 @@ router.get("/",restrict(), async (req, res, next) => {
     }
 })
 
-// get plants by ID (this works)
+// get plants by ID
 router.get("/:id", restrict(), async (req, res, next) => {
     try {
         const plant = await Plants.findById(req.params.id)
@@ -30,7 +30,7 @@ router.get("/:id", restrict(), async (req, res, next) => {
 })
 
 
-// get list of plants by user id (this should work)
+// get list of plants by user id
 router.get("/:id/plantsList", restrict(), async (req, res, next) => {
     const { id } = req.params
     Plants.getPlantsList(id)
@@ -43,28 +43,72 @@ router.get("/:id/plantsList", restrict(), async (req, res, next) => {
 })
 
 
-// // add plant to user
-// router.post("/:id/plantsList", async (req, res, next) => {
+// add plant to user - needs user_id in req to post
+router.post("/", async (req, res, next) => {
+    try {
+        Plants.add(req.body)
+            .then(newPlant => {
+                res.status(200).json(newPlant)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "Could not add plant"
+                })
+            })
+    } catch (err) {
+        next(err)
+    }
+})
 
-//     const { nickname, species, h2oFrequency } = req.body
 
-//     try {
-//         const newPlant = await Plants.add({
-//             nickname,
-//             species,
-//             h2oFrequency,
-//             // user id?
-//         })
-//     } catch(err) {
-//         next(err)
-//     }
-// })
-
-
-// edit plant
-
+// edit plant (working but not returning error message)
+router.put("/:id", restrict(), async (req, res, next) => {
+    try {
+        Plants.update(req.params.id, req.body)
+            .then(updatedPlant => {
+                if(updatedPlant) {
+                    res.json(updatedPlant)
+                } else {
+                    res.status(404).json({
+                        message: "Could not find plant with given ID"
+                    })
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "Failed to update plant"
+                })
+            })
+    } catch(err) {
+        next(err)
+    }
+})
 
 
 // delete plant
+router.delete("/:id", restrict(), async (req, res, next) => {
+    try {
+        Plants.remove(req.params.id)
+            .then(deleted => {
+                if(deleted) {
+                    res.json({ 
+                        message: "Deleted!"
+                    })
+                } else {
+                    res.status(404).json({
+                        message: "Could not find plant with given ID"
+                    })
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "Failed to delete plant"
+                })
+            })
+} catch(err) {
+    next(err)
+}
+})
+
 
 module.exports = router
